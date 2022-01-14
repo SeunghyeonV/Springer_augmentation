75175a7ededf59ababd97fa7d6f9e3faeb9d5188
SeunghyeonV <95251242+SeunghyeonV@users.noreply.github.com>
2022-01-14 15:32:39 -0500

    Update CIFAR100_advantage_plot_10sample.py

--- graph_utils/CIFAR100_advantage_plot_10sample.py
+++ graph_utils/CIFAR100_advantage_plot_10sample.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 
 # Colors for bar graphs
 color_list = ['#31a354', '#31a354', '#e377c2', 'sandybrown', '#ffed6f', '#9edae5', '#f7b6d2', 'sandybrown', '#74c476', '#a1d99b']
+augmnetations = ["Solarize (M2)", "Solarize (M2)", "Equalize (M5)", "Equalize (M8)", 
+                 "Solarize (M8)", "Solarize (M2)", "Posterize (M7)", "Equalize (M8)",
+                 "Brightness (M9)", "Equalize (M8)"]
 
 #model accuracy
 labels = ['Aquarium fish', 'Beaver', 'Beetle', 'Boy', 'Can', 'Motorcycle', 'Orchid', 'Rose', 'Sea', 'Tank']
-Aug_MA_values = [40, 26.67, 56.67, 40, 46.67, 66.67, 63.33, 46.67, 66.67, 50]
-DP_MA_values = [-48,   -11,   -34,-11,   -29,   -64,   -62,-46.67,   -62,-53]
+Aug_MA_values = [46.67, 26.67, 56.67, 40, 56.67, 90, 86.67, 46.67, 66.67, 50]
+DP_MA_values = [-48,   -11,   -34,-11,   -29,   -64,   -62,   -47,   -62,-53]
 CIFAR10_MA_advantage = [-8.00, 15.67, 22.67, 29.00, 17.67, 2.67, 1.33, 0, 4.67, -3]
 
 df_MA = pd.DataFrame({'DP_MA_values': DP_MA_values, 'Aug_MA_values': Aug_MA_values})
@@ -22,23 +25,44 @@ plt.bar(df_MA.index, df_MA['DP_MA_values'], color = '#1f77b4', width = 0.6)
 
 # x and y limits
 plt.xlim(-0.9, 10)
-plt.ylim(-90, 90)
+plt.ylim(-100, 100)
 
 # remove spines
 ax.spines['right'].set_visible(False)
 ax.spines['left'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
-ax.text(-0.18, -54, '-8.0', fontsize=12, c='k')
+
+ax.text(-0.5, -59, 'Solarize (M2)', fontsize=12, c='k')
+ax.text(-0.18, -54, '-1.67', fontsize=12, c='k')
+
+ax.text(0.46, 34, 'Solarize (M2)', fontsize=12, c='k')
 ax.text(0.69, 29, '+15.67', fontsize=12, c='k')
+
+ax.text(1.45, 65, 'Equalize (M5)', fontsize=12, c='k')
 ax.text(1.69, 59, '+22.67', fontsize=12, c='k')
+
+ax.text(2.45, 47, 'Equalize (M8)', fontsize=12, c='k')
 ax.text(2.73, 42, '+29.0', fontsize=12, c='k')
-ax.text(3.68, 48, '+17.67', fontsize=12, c='k')
-ax.text(4.75, 69, '+2.67', fontsize=12, c='k')
-ax.text(5.75, 65, '+1.33', fontsize=12, c='k')
-ax.text(6.77, 48, '+0.0', fontsize=12, c='k')
-ax.text(7.83, +69, '4.67', fontsize=12, c='k')
+
+ax.text(3.45, 64, 'Solarize (M8)', fontsize=12, c='k')
+ax.text(3.7, 58, '+27.67', fontsize=12, c='k')
+
+ax.text(4.5, 95, 'Solarize (M2)', fontsize=12, c='k')
+ax.text(4.75, 90, '+26.0', fontsize=12, c='k')
+
+ax.text(5.44, 88, 'Posterize (M7)', fontsize=12, c='k')
+ax.text(5.7, 82, '+24.67', fontsize=12, c='k')
+
+ax.text(6.45, 53, 'Equalize (M9)', fontsize=12, c='k')
+ax.text(6.79, 48, '-0.33', fontsize=12, c='k')
+
+ax.text(7.4, 74, 'Brightness (M9)', fontsize=12, c='k')
+ax.text(7.83, 68, '4.67', fontsize=12, c='k')
+
+ax.text(8.42, -64, 'Equalize (M8)', fontsize=12, c='k')
 ax.text(8.82, -59, '-3.0', fontsize=12, c='k')
+
 ax.text(1.4, -60, 'Higher MA: Better Utility', fontsize=12, c='k', size='xx-large', bbox=dict(facecolor='none', edgecolor='black', boxstyle='round,pad=0.5'))
 
 
@@ -52,19 +76,18 @@ plt.xticks(df_MA.index , labels = xticks_labels, fontsize=13)
 
 # title and legend
 legend_label = ['Advantages', 'Aug_MA_values (colors)', 'DP_MA_values']
-plt.legend(legend_label, ncol = 3, bbox_to_anchor=([0.75, 1.06, 0, 0]), frameon = True, fontsize=10)
+plt.legend(legend_label, ncol = 3, bbox_to_anchor=([0.66, 1.06, 0, 0]), frameon = True, fontsize=10)
 plt.title('CIFAR-100 Best Augmentation Advantages Per Label (Model Accuracy) \n', loc='center', fontdict = {'fontsize' : 20})
-# plt.savefig("C:/Users/seacl/Desktop/MA_advantages.png", dpi=300, bbox_inches = 'tight')
+plt.savefig("C:/Users/seacl/Desktop/CIFAR100_MA_advantages.png", dpi=300, bbox_inches = 'tight')
 
 
 
 #############################################################################
 
 
-
 # attack accuracy
-Aug_AA_values = [3.33, 6.67, 10, 6.67, 3.33, 3.33, 6.67, 20, 6.67, 3.33]
-DP_AA_values = [-20, -6.67, -6.67, -10, -10, -23.33, -46.67, -26.67, -6.67, -3.33]
+Aug_AA_values = [3.33, 6.67,    10, 3.33, 3.33,   3.33,   20,     20,  6.67,  3.33]
+DP_AA_values = [  -20,-6.67, -6.67,  -10,  -10, -23.33, -46.67, -26.67, -6.67, -3.33]
 CIFAR10_AA_advantage = [16.67, 0. -3.33, 3.33, 6.67, 20, 40, 6.67, 0, 0]
 
 
@@ -86,16 +109,37 @@ ax.spines['right'].set_visible(False)
 ax.spines['left'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
-ax.text(-0.31, 4.5, '+16.67', fontsize=12, c='k')
-ax.text(0.8, 8, '+0.0', fontsize=12, c='k')
+
+ax.text(-0.53, 7, 'Solarize (M2)', fontsize=12, c='k')
+ax.text(-0.31, 4.3, '+16.67', fontsize=12, c='k')
+
+ax.text(0.43, 10.5, 'Solarize (M2)', fontsize=12, c='k')
+ax.text(0.8, 7.5, '±0.0', fontsize=12, c='k')
+
+ax.text(1.45,0 -12, 'Equalize (M5)', fontsize=12, c='k')
 ax.text(1.78, -9.5, '-3.33', fontsize=12, c='k')
-ax.text(2.73, 8, '+3.33', fontsize=12, c='k')
-ax.text(3.77, 5, '+6.67', fontsize=12, c='k')
-ax.text(4.76, 5, '+20.0', fontsize=12, c='k')
-ax.text(5.75, 8, '+40.0', fontsize=12, c='k')
-ax.text(6.75, 21, '+6.67', fontsize=12, c='k')
-ax.text(7.75, 7.5, '+0.0', fontsize=12, c='k')
-ax.text(8.8, 4.5, '+0.0', fontsize=12, c='k')
+
+ax.text(2.45, 7.7, 'Equalize (M8)', fontsize=12, c='k')
+ax.text(2.73, 4.5, '+6.67', fontsize=12, c='k')
+
+ax.text(3.4, 4, 'Solarize (M8)', fontsize=12, c='k')
+ax.text(3.77, 1, '+6.67', fontsize=12, c='k')
+
+ax.text(4.5, 8, 'Solarize (M2)', fontsize=12, c='k')
+ax.text(4.73, 5.5, '+20.0', fontsize=12, c='k')
+
+ax.text(5.44, 24, 'Posterize (M7)', fontsize=12, c='k')
+ax.text(5.7, 21, '+26.67', fontsize=12, c='k')
+
+ax.text(6.45, 20.5, 'Equalize (M9)', fontsize=12, c='k')
+ax.text(6.75, 18, '+6.67', fontsize=12, c='k')
+
+ax.text(7.4, 10, 'Brightness (M9)', fontsize=12, c='k')
+ax.text(7.75, 7.5, '±0.0', fontsize=12, c='k')
+
+ax.text(8.42, 7, 'Equalize (M8)', fontsize=12, c='k')
+ax.text(8.8, 4.5, '±0.0', fontsize=12, c='k')
+
 ax.text(1.4, -35, 'Lower AA: Better Privacy', fontsize=12, c='k', size='xx-large', bbox=dict(facecolor='none', edgecolor='black', boxstyle='round,pad=0.5'))
 
 #grid
@@ -108,11 +152,9 @@ plt.xticks(df_AA.index , labels = xticks_labels, fontsize=13)
 
 # title and legend
 legend_label = ['Advantages', 'Aug_AA_values (colors)', 'DP_AA_values']
-plt.legend(legend_label, ncol = 3, bbox_to_anchor=([0.75, 1.06, 0, 0]), frameon = True, fontsize=10)
+plt.legend(legend_label, ncol = 3, bbox_to_anchor=([0.66, 1.06, 0, 0]), frameon = True, fontsize=10)
 plt.title('CIFAR-100 Best Augmentation Advantages Per Label (Attack Accuracy) \n', loc='center', fontdict = {'fontsize' : 20})
-# plt.savefig("C:/Users/seacl/Desktop/AA_advantages.png", dpi=300, bbox_inches = 'tight')
+plt.savefig("C:/Users/seacl/Desktop/CIFAR100_AA_advantages.png", dpi=300, bbox_inches = 'tight')
 
 
 plt.show()
-
-# total_advantage_score = []
