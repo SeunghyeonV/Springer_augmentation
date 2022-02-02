43d026330c4951dfa134dcee3f686ed1296b36ae
SeunghyeonV <95251242+SeunghyeonV@users.noreply.github.com>
2022-02-01 19:57:52 -0500

    Update CIFAR10_linear_graphs.py

--- graph_utils/CIFAR10_linear_graphs.py
+++ graph_utils/CIFAR10_linear_graphs.py
@@ -2,6 +2,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import matplotlib.font_manager as font_manager
+
+# all font
+csfont = {'fontname':'Times New Roman'}
+# legend font
+font = font_manager.FontProperties(family='Times New Roman',
+                                   style='normal', size=10)
 
 ## Draw linear graphs for each augmentation that shows accuracy change per magnitude
 
@@ -42,81 +49,82 @@ Magnitude = [1,3,5,7,9]
 plt.xticks(Magnitude, Magnitude)
 plt.ylim(10,100)
 
-plt.title("CIFAR-10 Model accuracy range")
-plt.xlabel("Magnitude")
-plt.ylabel("Accuracy")
+plt.title("CIFAR-10 Model accuracy range", **csfont, fontsize=14)
+plt.xlabel("Magnitude", **csfont)
+plt.ylabel("Accuracy", **csfont)
 x_axis = range(0, 15)
 # cmap = plt.get_cmap('tab20c') #automatically adjust color by applying color=cmap(i)
 
+# highest value and coordinates
 original_MA_max = max(original_MA)
 original_MA_max_index = x_axis[original_MA.index(original_MA_max)]
 original_MA_mx_coord = 7.6
 original_MA_my_coord = original_MA_max+9
-plt.annotate("[O]{}%".format(original_MA_max), xy=(original_MA_max_index, original_MA_max), xytext=(original_MA_mx_coord,original_MA_my_coord))
+plt.annotate("[O]{}%".format(original_MA_max), xy=(original_MA_max_index, original_MA_max), xytext=(original_MA_mx_coord,original_MA_my_coord), **csfont)
 
-
+# highest value and coordinates
 autocontrast_MA_min = min(autocontrast_MA)
 autocontrast_MA_min_index = x_axis[autocontrast_MA.index(autocontrast_MA_min)]
 autocontrast_MA_mx_coord = 7.63
 autocontrast_MA_my_coord = autocontrast_MA_min+3
 
-
+# highest value and coordinates
 translateY_MA_min = min(translateY_MA)
 translateY_MA_min_index = x_axis[translateY_MA.index(translateY_MA_min)]
 translateY_MA_mx_coord = 7.4
 translateY_MA_my_coord = translateY_MA_min-6
 
 
-plt.plot(Magnitude, original_MA, label="original", color='red')
+plt.plot(Magnitude, original_MA, label="Original", color='red')
 plt.scatter(Magnitude, original_MA, c=['red'])
 
-plt.plot(Magnitude, autocontrast_MA, label="autocontrast", color='navy')
+plt.plot(Magnitude, autocontrast_MA, label="Autocontrast", color='navy')
 plt.scatter(Magnitude, autocontrast_MA, c='navy')
-plt.annotate("[A]{}%".format(autocontrast_MA_min), xy=(autocontrast_MA_min_index, autocontrast_MA_min), xytext=(autocontrast_MA_mx_coord,autocontrast_MA_my_coord))
+plt.annotate("[A]{}%".format(autocontrast_MA_min), xy=(autocontrast_MA_min_index, autocontrast_MA_min), xytext=(autocontrast_MA_mx_coord,autocontrast_MA_my_coord), **csfont)
 
-plt.plot(Magnitude, brightness_MA, label="brightness", color='#ffed6f')
+plt.plot(Magnitude, brightness_MA, label="Brightness", color='#ffed6f')
 plt.scatter(Magnitude, brightness_MA, c='#ffed6f')
 
-plt.plot(Magnitude, color_MA, label="color", color='royalblue')
+plt.plot(Magnitude, color_MA, label="Color", color='royalblue')
 plt.scatter(Magnitude, color_MA, c='royalblue')
 
-plt.plot(Magnitude, contrast_MA, label="contrast", color='#e377c2')
+plt.plot(Magnitude, contrast_MA, label="Contrast", color='#e377c2')
 plt.scatter(Magnitude, contrast_MA, c='#e377c2')
 
-plt.plot(Magnitude, equalize_MA, label="equalize", color='#f7b6d2')
+plt.plot(Magnitude, equalize_MA, label="Equalize", color='#f7b6d2')
 plt.scatter(Magnitude, equalize_MA, c='#f7b6d2')
 
-plt.plot(Magnitude, invert_MA, label="invert", color='orangered')
+plt.plot(Magnitude, invert_MA, label="Invert", color='orangered')
 plt.scatter(Magnitude, invert_MA, c='orangered')
 
-plt.plot(Magnitude, posterize_MA, label="posterize", color='sandybrown')
+plt.plot(Magnitude, posterize_MA, label="Posterize", color='sandybrown')
 plt.scatter(Magnitude, posterize_MA, c='sandybrown')
 
-plt.plot(Magnitude, rotate_MA, label="rotate", color='tan')
+plt.plot(Magnitude, rotate_MA, label="Rotate", color='tan')
 plt.scatter(Magnitude, rotate_MA, c='tan')
 
-plt.plot(Magnitude, sharpness_MA, label="sharpness", color='#6b6ecf')
+plt.plot(Magnitude, sharpness_MA, label="Sharpness", color='#6b6ecf')
 plt.scatter(Magnitude, sharpness_MA, c='#6b6ecf')
 
-plt.plot(Magnitude, shearX_MA, label="shearX", color='mediumseagreen')
+plt.plot(Magnitude, shearX_MA, label="ShearX", color='mediumseagreen')
 plt.scatter(Magnitude, shearX_MA, c='mediumseagreen')
 
-plt.plot(Magnitude, shearY_MA, label="shearY", color='mediumaquamarine')
+plt.plot(Magnitude, shearY_MA, label="ShearY", color='mediumaquamarine')
 plt.scatter(Magnitude, shearY_MA, c='mediumaquamarine')
 
-plt.plot(Magnitude, solarize_MA, label="solarize", color='#31a354')
+plt.plot(Magnitude, solarize_MA, label="Solarize", color='#31a354')
 plt.scatter(Magnitude, solarize_MA, c='#31a354')
 
-plt.plot(Magnitude, translateX_MA, label="translateX", color='orchid')
+plt.plot(Magnitude, translateX_MA, label="TranslateX", color='orchid')
 plt.scatter(Magnitude, translateX_MA, c='orchid')
 
-plt.plot(Magnitude, translateY_MA, label="translateY", color='pink')
+plt.plot(Magnitude, translateY_MA, label="TranslateY", color='pink')
 plt.scatter(Magnitude, translateY_MA, c='pink')
-plt.annotate("[TY]{}%".format(translateY_MA_min), xy=(translateY_MA_min_index, translateY_MA_min), xytext=(translateY_MA_mx_coord,translateY_MA_my_coord))
+plt.annotate("[TY]{}%".format(translateY_MA_min), xy=(translateY_MA_min_index, translateY_MA_min), xytext=(translateY_MA_mx_coord,translateY_MA_my_coord), **csfont)
 
-plt.legend(bbox_to_anchor=(1.0, 1.0))
+plt.legend(bbox_to_anchor=(1.0, 1.0), prop=font)
 plt.tight_layout()
-plt.savefig("C:/Users/seacl/Desktop/Fig7.png", dpi=300)
+plt.savefig("C:/Users/seacl/Desktop/cifar10_linear_MA.png", dpi=200)
 
 plt.show()
 
@@ -125,79 +133,81 @@ Magnitude = [1,3,5,7,9]
 plt.xticks(Magnitude, Magnitude)
 plt.ylim(10,100)
 
-plt.title("CIFAR-10 Attack accuracy range")
-plt.xlabel("Magnitude")
-plt.ylabel("Accuracy")
+plt.title("CIFAR-10 Attack accuracy range", **csfont, fontsize=14)
+plt.xlabel("Magnitude", **csfont)
+plt.ylabel("Accuracy", **csfont)
 x_axis = range(0, 15)
 # cmap = plt.get_cmap('tab20c') #automatically adjust color by applying color=cmap(i)
 
+# highest value and coordinates
 original_AA_max = max(original_AA)
 original_AA_max_index = x_axis[original_AA.index(original_AA_max)]
 original_AA_mx_coord = 7.6
 original_AA_my_coord = original_AA_max+8
 
-
+# highest value and coordinates
 autocontrast_AA_min = min(autocontrast_AA)
 autocontrast_AA_min_index = x_axis[autocontrast_AA.index(autocontrast_AA_min)]
 autocontrast_AA_mx_coord = 7.6
 autocontrast_AA_my_coord = autocontrast_AA_min+6.5
 
+# highest value and coordinates
 translateY_AA_min = min(translateY_AA)
 translateY_AA_min_index = x_axis[translateY_AA.index(translateY_AA_min)]
 translateY_AA_mx_coord = 7.4
 translateY_AA_my_coord = translateY_AA_min-5
 
-plt.plot(Magnitude, original_AA, label="original", color='red')
+plt.plot(Magnitude, original_AA, label="Original", color='red')
 plt.scatter(Magnitude, original_AA, c=['red'])
-plt.annotate("[O]{}%".format(original_AA_max), xy=(original_AA_max_index, original_AA_max), xytext=(original_AA_mx_coord,original_AA_my_coord))
+plt.annotate("[O]{}%".format(original_AA_max), xy=(original_AA_max_index, original_AA_max), xytext=(original_AA_mx_coord,original_AA_my_coord), **csfont)
 
 
-plt.plot(Magnitude, autocontrast_AA, label="autocontrast", color='navy') 
+plt.plot(Magnitude, autocontrast_AA, label="Autocontrast", color='navy') 
 plt.scatter(Magnitude, autocontrast_AA, c='navy')
-plt.annotate("[A]{}%".format(autocontrast_AA_min), xy=(autocontrast_AA_min_index, autocontrast_AA_min), xytext=(autocontrast_AA_mx_coord,autocontrast_AA_my_coord))
+plt.annotate("[A]{}%".format(autocontrast_AA_min), xy=(autocontrast_AA_min_index, autocontrast_AA_min), xytext=(autocontrast_AA_mx_coord,autocontrast_AA_my_coord), **csfont)
 
-plt.plot(Magnitude, brightness_AA, label="brightness", color='#ffed6f')
+plt.plot(Magnitude, brightness_AA, label="Brightness", color='#ffed6f')
 plt.scatter(Magnitude, brightness_AA, c='#ffed6f')
 
-plt.plot(Magnitude, color_AA, label="color", color='royalblue')
+plt.plot(Magnitude, color_AA, label="Color", color='royalblue')
 plt.scatter(Magnitude, color_AA, c='royalblue')
 
-plt.plot(Magnitude, contrast_AA, label="contrast", color='#e377c2') 
+plt.plot(Magnitude, contrast_AA, label="Contrast", color='#e377c2') 
 plt.scatter(Magnitude, contrast_AA, c='#e377c2') 
 
-plt.plot(Magnitude, equalize_AA, label="equalize", color='#f7b6d2')
+plt.plot(Magnitude, equalize_AA, label="Equalize", color='#f7b6d2')
 plt.scatter(Magnitude, equalize_AA, c='#f7b6d2')
 
-plt.plot(Magnitude, invert_AA, label="invert", color='orangered')
+plt.plot(Magnitude, invert_AA, label="Invert", color='orangered')
 plt.scatter(Magnitude, invert_AA, c='orangered')
 
-plt.plot(Magnitude, posterize_AA, label="posterize", color='sandybrown')
+plt.plot(Magnitude, posterize_AA, label="Posterize", color='sandybrown')
 plt.scatter(Magnitude, posterize_AA, c='sandybrown')
 
-plt.plot(Magnitude, rotate_AA, label="rotate", color='tan')
+plt.plot(Magnitude, rotate_AA, label="Rotate", color='tan')
 plt.scatter(Magnitude, rotate_AA, c='tan')
 
-plt.plot(Magnitude, sharpness_AA, label="sharpness", color='#6b6ecf') 
+plt.plot(Magnitude, sharpness_AA, label="Sharpness", color='#6b6ecf') 
 plt.scatter(Magnitude, sharpness_AA, c='#6b6ecf') 
 
-plt.plot(Magnitude, shearX_AA, label="shearX", color='mediumseagreen' )
+plt.plot(Magnitude, shearX_AA, label="ShearX", color='mediumseagreen' )
 plt.scatter(Magnitude, shearX_AA, c='mediumseagreen')
 
-plt.plot(Magnitude, shearY_AA, label="shearY", color='mediumaquamarine') 
+plt.plot(Magnitude, shearY_AA, label="ShearY", color='mediumaquamarine') 
 plt.scatter(Magnitude, shearY_AA, c='mediumaquamarine')
 
-plt.plot(Magnitude, solarize_AA, label="solarize", color='#31a354')
+plt.plot(Magnitude, solarize_AA, label="Solarize", color='#31a354')
 plt.scatter(Magnitude, solarize_AA, c='#31a354')
 
-plt.plot(Magnitude, translateX_AA, label="translateX", color='orchid')
+plt.plot(Magnitude, translateX_AA, label="TranslateX", color='orchid')
 plt.scatter(Magnitude, translateX_AA, c='orchid')
 
-plt.plot(Magnitude, translateY_AA, label="translateY", color='pink')
+plt.plot(Magnitude, translateY_AA, label="TranslateY", color='pink')
 plt.scatter(Magnitude, translateY_AA, c='pink')
-plt.annotate("[TY]{}%".format(translateY_AA_min), xy=(translateY_AA_min_index, translateY_AA_min), xytext=(translateY_AA_mx_coord,translateY_AA_my_coord))
+plt.annotate("[TY]{}%".format(translateY_AA_min), xy=(translateY_AA_min_index, translateY_AA_min), xytext=(translateY_AA_mx_coord,translateY_AA_my_coord), **csfont)
 
-plt.legend(bbox_to_anchor=(1.0, 1.0))
+plt.legend(bbox_to_anchor=(1.0, 1.0), prop=font)
 plt.tight_layout()
-plt.savefig("C:/Users/seacl/Desktop/Fig8.png", dpi=300)
+plt.savefig("C:/Users/seacl/Desktop/cifar10_linear_AA.png", dpi=200)
 
 plt.show()
